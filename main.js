45640a5865c149ac1884f6cea288b959000a2048
Kshitij2011 <77619958+Kshitij2011@users.noreply.github.com>
2021-07-29 17:25:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-noseX=0;
-noseY=0;
 
 function preload(){
-    clown_nose = loadImage('https://i.pinimg.com/originals/6b/66/7e/6b667e797a766356281aabdf4b5f1e86.png');
+   
 }
 
 function setup(){
-    canvas = creatCanvas(300, 300);
+    canvas = createCanvas(300, 300);
     canvas.center();
-    video = creatCapture(VIDEO);
+    video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose, gotPoses');
+    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded(){
@@ -25,17 +23,15 @@ function gotPoses(results)
     if (results.length > 0)
     {
         console.log(results);
-        noseX = results[0].pose.nose.x-15;
-        noseY = results[0].pose.nose.y-15;
+        console.log("nose x = " + results[0].pose.nose.x);
+       console.log("nose y = " + results[0].pose.nose.y); 
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
-    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
     save('myFilterImage.png');
 }
-
